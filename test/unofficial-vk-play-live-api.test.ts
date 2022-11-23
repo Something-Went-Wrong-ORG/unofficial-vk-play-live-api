de8ff8d0e6843ac4f0e9835bd78c79ac46792b4f
Mikhail <profiler4100v2@gmail.com>
2022-11-23 03:48:16 +0200

    Small tests refactoring

--- test/unofficial-vk-play-live-api.test.ts
+++ test/unofficial-vk-play-live-api.test.ts
@@ -14,29 +14,23 @@ import VKPLAPI from '../src/unofficial-vk-play-live-api';
  * Dummy test
  */
 describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy();
-  });
+  const streamManager = VKPLAPI.GetStreamManager('d3l_ru');
 
-  it('DummyClass is instantiable', () => {
+  it('Root class is instantiable', () => {
     expect(new VKPLAPI()).toBeInstanceOf(VKPLAPI);
   });
 
-  it('DummyClass is instantiable', () => {
-    expect(new VKPLAPI()).toBeInstanceOf(VKPLAPI);
-  });
-
-  it('Request must work', async () => {
-    const streamManager = VKPLAPI.GetStreamManager('d3l_ru');
-
+  it('Metadata must come from server', async () => {
     const metadata = await streamManager.GetMetadata();
     expect(metadata).not.toBeNull();
     // todo: fix and enable check
     // expect(fieldsAreMatchingWithType(metadata, keyof<Metadata>)).toBeTruthy();
+  });
 
-    const embedMetadata = await streamManager.GetPlayerOptions();
-    expect(embedMetadata).not.toBeNull();
+  it('PlayerOptions must come from server', async () => {
+    const playerOptions = await streamManager.GetPlayerOptions();
+    expect(playerOptions).not.toBeNull();
     // todo: fix and enable check
-    // expect(fieldsAreMatchingWithType(embedMetadata, PlayerOptions)).toBeTruthy();
+    // expect(fieldsAreMatchingWithType(playerOptions, PlayerOptions)).toBeTruthy();
   });
 });
